fa1c3f189b01334061b4fa393a00f99da9828f71
gab <gabrielfontes321@gmail.com>
2022-03-30 19:39:36 -0300

    feat: adding routes in index

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
+const express = require('express');
+const productRouter = require('./routes/productRouter');
+const salesRouter = require('./routes/salesRouter');
+
+const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productRouter);
+
+app.use('/sales', salesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
