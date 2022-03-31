1fff3ac7ab6981c8cd03cc4a3f0fdb99729418a2
gab <gabrielfontes321@gmail.com>
2022-03-31 20:44:38 -0300

    test: initiating tests requirement 1

---
+++ test/unit/models/productsModels.js
@@ -0,0 +1,8 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const connection = require('../../../models/connection');
+const productModel = require('../../../models/productModel');
+
+
+describe('');                                                                                
