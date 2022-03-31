f6d6eb390e9ef84a7300aed0f6e0837e052e032c
gab <gabrielfontes321@gmail.com>
2022-03-30 22:27:56 -0300

    feat: implementing verify if product exists model and create product service

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,11 @@
 const connection = require('./connection');
 
+const verifyProductExists = async ({ name }) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE name = ?;';
+  const [product] = await connection.execute(query, [name]);
+  return product;
+};
+
 const list = async () => {
   const query = 'SELECT * FROM StoreManager.products ORDER BY id';
   const [products] = await connection.execute(query);
@@ -12,7 +18,19 @@ const listById = async (id) => {
   return product;
 };
 
+const create = async ({ name, quantity }) => {
+  const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)';
+  const [{ insertId }] = await connection.execute(query, [name, quantity]);
+  return {
+    id: insertId,
+    name,
+    quantity,
+  };
+};
+
 module.exports = {
   list,
   listById,
+  create,
+  verifyProductExists,
 };
