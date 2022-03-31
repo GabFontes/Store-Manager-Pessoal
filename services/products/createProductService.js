9c7c44ca76cdcb45083c7be57678dce788d52bf7
gab <gabrielfontes321@gmail.com>
2022-03-30 23:56:50 -0300

    feat: fixing function name "verify Product Exists Name" and middleware order error

--- services/products/createProductService.js
+++ services/products/createProductService.js
@@ -1,7 +1,7 @@
 const productModel = require('../../models/productModel');
 
 const createProductService = async (product) => {
-  const productExists = await productModel.verifyProductExists(product);
+  const productExists = await productModel.verifyProductExistsName(product);
   if (productExists.length) {
     throw new Error('Product already exists');
   }
