ad0655b06d2495478f5bd9113deb652ee5971ade
gab <gabrielfontes321@gmail.com>
2022-03-30 22:26:48 -0300

    feat: implementing post route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,11 @@
 const productRouter = require('express').Router();
 const productController = require('../controllers/products');
+const { validateName, validateQuantity } = require('../middlewares/validateProductMiddleware');
 
 productRouter.get('/', productController.list);
+
+productRouter.post('/', validateName, validateQuantity,
+  productController.create);
 
 productRouter.get('/:id', productController.listById);
 
